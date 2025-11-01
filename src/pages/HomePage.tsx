3f07aa62d6ae5fcbea1b8379e28fdf6f15a9641d
cadst <tyu1234567@naver.com>
2025-11-01 18:19:41 +0900

    [fix] 홈페이지 typescript 적용

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+
+type Item = {
+  title: string;
+  description: string;
+};
+
+let items: Item[];
 
 const HomePage = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-
   useEffect(() => {
-    setTitle(localStorage.getItem("title") || "");
-    setDescription(localStorage.getItem("description") || "");
-  }, []);
+    items = JSON.parse(localStorage.getItem("item") || "[]");
+  }, [items]);
 
   return (
     <>
       <h1>Home Page</h1>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      {/* <h2>{title}</h2>
+      <p>{description}</p> */}
       <Link to="/edit">EDIT</Link>
       <Link to="/edit?mode=create">CREATE</Link>
     </>
