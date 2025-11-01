79582784411a439b2a60a6c2d53d29c4b753cbb7
cadst <tyu1234567@naver.com>
2025-11-01 15:53:28 +0900

    [chore] 홈페이지 주석 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,14 +15,8 @@ const HomePage = () => {
       <h1>Home Page</h1>
       <h2>{title}</h2>
       <p>{description}</p>
-      <Link to="/edit">
-        <button>EDIT</button>
-      </Link>
-      <Link to="/edit?mode=create">
-        <button>CREATE</button>
-      </Link>
-      {/* useNaviate: 동적 처리 (예: 특정 조건에서 이동) 할 때 사용
-        Link: 정적 처리 (예: 버튼, 링크 클릭 시 이동) 할 때 사용  */}
+      <Link to="/edit">EDIT</Link>
+      <Link to="/edit?mode=create">CREATE</Link>
     </>
   );
 };
