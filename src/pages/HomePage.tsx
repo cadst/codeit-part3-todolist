6809d50d7d2dfa409a92ee186c91fd274f4ce5db
cadst <tyu1234567@naver.com>
2025-11-01 15:51:49 +0900

    Revert "[chore] 홈페이지 주석 추가"
    This reverts commit 5f77727de863062272b235fa6ef4486a314138af.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,15 +12,17 @@ const HomePage = () => {
 
   return (
     <>
-      {/* useNavigate: 동적 처리 (예: 특정 조건에서 이동) 할 때 사용
-        Link: 정적 처리 (예: 버튼, 링크 클릭 시 이동) 할 때 사용  
-        둘 다 내부적으로 CSR 방식으로 작동 (배포 시 설정 추가 하지 않으면 오류 발생)
-        */}
       <h1>Home Page</h1>
       <h2>{title}</h2>
       <p>{description}</p>
-      <Link to="/edit">EDIT</Link>
-      <Link to="/edit?mode=create">CREATE</Link>
+      <Link to="/edit">
+        <button>EDIT</button>
+      </Link>
+      <Link to="/edit?mode=create">
+        <button>CREATE</button>
+      </Link>
+      {/* useNaviate: 동적 처리 (예: 특정 조건에서 이동) 할 때 사용
+        Link: 정적 처리 (예: 버튼, 링크 클릭 시 이동) 할 때 사용  */}
     </>
   );
 };
