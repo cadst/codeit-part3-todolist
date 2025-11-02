888b991f3e59cbf6e00724de166bd7d254cde56d
cadst <tyu1234567@naver.com>
2025-11-02 23:12:38 +0900

    [chore] wsl 사용시 dev 모드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true, // Windows/WSL 환경에서 파일 변경 감지
+    },
+    hmr: {
+      overlay: true, // 에러 오버레이 표시
+    },
+  },
+});
