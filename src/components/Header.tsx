56c6638a10ca7aebf352f31989f90acd8b52d2c9
cadst <tyu1234567@naver.com>
2025-11-04 21:02:36 +0900

    [chore] 주석 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav>
-      {/* 시맨틱 태그 */}
       <Link to="/">Home</Link>
       <Link to="/edit">Edit</Link>
     </nav>
